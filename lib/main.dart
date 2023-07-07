6cfe1df7da9ef57477bd621cac24bc1760de42b0
Juan Pablo <juanpablo@meowtel.com>
2023-07-07 12:59:03 -0300

    stream

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,26 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
 import 'package:chat_app/bloc/auth_bloc.dart';
 import 'package:chat_app/repositories/auth_repository.dart';
 import 'package:chat_app/screens/auth.dart';
 import 'package:chat_app/screens/chat.dart';
-import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
+
 import 'firebase_options.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
   runApp(
     MultiBlocProvider(
       providers: [
         BlocProvider(
-          create: (context) => AuthBloc(authRepository),
+          create: (context) => AuthBloc(AuthRepository()),
         )
       ],
       child: const App(),
@@ -24,7 +28,7 @@ void main() async {
   );
 }
 
-final AuthRepository authRepository = AuthRepository();
+final firebaseAuthStream = FirebaseAuth.instance.authStateChanges();
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -32,7 +36,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<AuthBloc, AuthState>(
-      builder: (context, state) {
+      builder: (blocContext, state) {
         return MaterialApp(
           title: 'FlutterChat',
           theme: ThemeData().copyWith(
@@ -40,21 +44,27 @@ class App extends StatelessWidget {
             colorScheme: ColorScheme.fromSeed(
                 seedColor: const Color.fromARGB(255, 63, 17, 177)),
           ),
-          home: state is AuthenticatedState
-              ? const ChatScreen()
-              : const AuthScreen(),
+          home: StreamBuilder(
+            stream: firebaseAuthStream,
+            builder: (streamContext, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Text('Loading...');
+              }
+
+              if (snapshot.hasData && snapshot.data != null) {
+                context.read<AuthBloc>().emit(
+                      AuthenticatedState(user: snapshot.data!),
+                    );
+                return const ChatScreen();
+              }
+              context.read<AuthBloc>().emit(
+                    AuthUnathenticatedState(),
+                  );
+              return const AuthScreen();
+            },
+          ),
         );
       },
     );
   }
 }
-
-          // home: StreamBuilder(
-          //   stream: FirebaseAuth.instance.authStateChanges(),
-          //   builder: (context, snapshot) {
-          //     if (snapshot.hasData) {
-          //       return const ChatScreen();
-          //     }
-          //     return const AuthScreen();
-          //   },
-          // ),
